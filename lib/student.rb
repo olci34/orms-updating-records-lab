b56c989ca30343de9f9a9a77e3ac0179636a2a63
github username <github email address>
2021-02-11 15:10:39 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,64 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  attr_accessor :id, :name, :grade
+  def initialize(name, grade, id = nil)
+    self.name = name
+    self.grade = grade
+    self.id = id
+  end
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO students (name, grade) VALUES (?,?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    song = self.new(name,grade)
+    song.save
+    song
+  end
+
+  def self.new_from_db(row)
+    song = self.create(row[1], row[2])
+    song.id = row[0]
+    song
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM students WHERE name = ?
+    SQL
+    song_row = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(song_row)
+  end
+
+  def update
+    sql = <<-SQL
+    UPDATE students SET name = ?, grade = ? WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end
